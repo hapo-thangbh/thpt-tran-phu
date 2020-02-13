e9005754c775bae2cfa08ff0aaba2a6c1606ba04
Bui Huu Thang <thang.bui@codelovers.vn>
2020-02-13 14:14:48 +0700

    change name folder

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,15 +10,15 @@
     <!-- Font Awesome Icons -->
     <base href="{{ asset('') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ asset('admin/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('include/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{ asset('admin/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('include/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('/admin/plugins/select2/css/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/admin/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/dist/css/adminlte.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/dist/css/phuc.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/dist/css/bootstrap-select.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/include/plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/include/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('include/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('include/dist/css/phuc.css') }}">
+    <link rel="stylesheet" href="{{ asset('include/dist/css/bootstrap-select.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
@@ -44,37 +44,37 @@
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
 
-<script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('include/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap -->
-<script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('include/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- overlayScrollbars -->
-<script src="{{ asset('admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+<script src="{{ asset('include/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('admin/dist/js/adminlte.js') }}"></script>
+<script src="{{ asset('include/dist/js/adminlte.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
 <!-- OPTIONAL SCRIPTS -->
-<script src="{{ asset('admin/dist/js/demo.js') }}"></script>
+<script src="{{ asset('include/dist/js/demo.js') }}"></script>
 
 <!-- PAGE PLUGINS -->
 <!-- jQuery Mapael -->
-<script src="{{ asset('admin/plugins/raphael/raphael.min.js') }}"></script>
-<script src="{{ asset('admin/plugins/jquery-mapael/jquery.mapael.min.js') }}"></script>
-<script src="{{ asset('admin/plugins/jquery-mapael/maps/usa_states.min.js') }}"></script>
+<script src="{{ asset('include/plugins/raphael/raphael.min.js') }}"></script>
+<script src="{{ asset('include/plugins/jquery-mapael/jquery.mapael.min.js') }}"></script>
+<script src="{{ asset('include/plugins/jquery-mapael/maps/usa_states.min.js') }}"></script>
 <!-- ChartJS -->
-<script src="{{ asset('admin/plugins/chart.js/Chart.min.js') }}"></script>
-<script src="{{ asset('admin/plugins/select2/js/select2.full.min.js') }}"></script>
+<script src="{{ asset('include/plugins/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('include/plugins/select2/js/select2.full.min.js') }}"></script>
 
 <!-- PAGE SCRIPTS -->
-{{--<script src="{{ asset('admin/dist/js/pages/dashboard2.js') }}"></script>--}}
-<script src="{{ asset('admin/dist/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('admin/dist/js/bootstrap-select.min.js') }}"></script>
-<script src="{{ asset('/admin/plugins/select2/js/select2.full.min.js') }}"></script>
+{{--<script src="{{ asset('include/dist/js/pages/dashboard2.js') }}"></script>--}}
+<script src="{{ asset('include/dist/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('include/dist/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('/include/plugins/select2/js/select2.full.min.js') }}"></script>
 <script src="//cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/js/bootstrap-multiselect.min.js"></script>
-<script src="{{ asset('admin/dist/js/category.js') }}"></script>
-<script src="{{ asset('admin/dist/js/profile.js') }}"></script>
-<script src="{{ asset('admin/dist/js/post.js') }}"></script>
-<script src="{{ asset('admin/dist/js/page.js') }}"></script>
+<script src="{{ asset('include/dist/js/category.js') }}"></script>
+<script src="{{ asset('include/dist/js/profile.js') }}"></script>
+<script src="{{ asset('include/dist/js/post.js') }}"></script>
+<script src="{{ asset('include/dist/js/page.js') }}"></script>
 @yield('script')
 </body>
 </html>
