c18e5d8f7c1eda26d08e1c9e30d109e8ffe3a4ed
Bui Huu Thang <thang.bui@codelovers.vn>
2020-02-13 14:23:06 +0700

    púh

--- resources/views/admin/components/menu.blade.php
+++ resources/views/admin/components/menu.blade.php
@@ -1,8 +1,8 @@
-<!-- Main Sidebar Container -->
+﻿<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('indexAdmin') }}" class="brand-link">
-        <img src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('include/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">Welcome !</span>
     </a>
@@ -12,7 +12,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('include/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="{{ route('AdminProfile', \Illuminate\Support\Facades\Auth::id()) }}" class="d-block">{{ \Illuminate\Support\Facades\Auth::user()->name }}</a>
